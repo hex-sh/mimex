fb88c76c75c00acaeb4f39c31d93945b59f26632
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-09-29 14:15:39 +0200

    Add tests for `mime_type/1`

--- lib/mime.ex
+++ lib/mime.ex
@@ -1,14 +1,64 @@
 defmodule MIME do
+  # Read all the MIME types into a list of the following structure:
+  # [["sample/mime-type", "ext1", "ext2"], [..]]
   types = File.read!("#{:code.priv_dir(:mimex)}/mime.types")
   |> String.strip
   |> String.split("\n")
   |> Enum.map(&(String.split(&1, " ")))
 
   Enum.each types, fn [mimetype | extensions] ->
-    # We only support finding the first extension for a MIME type.
-    def extension(unquote(mimetype)), do: unquote("." <> hd(extensions))
+    def extension(unquote(mimetype)) do
+      # Return the first extension from the list, prepended by "."
+      unquote({:ok, "." <> hd(extensions)})
+    end
+
+    def extensions(unquote(mimetype)) do
+      # Return the list of extensions, all prepended by "."
+      unquote(
+        {:ok, Enum.map(extensions, fn ext -> ("." <> ext) end)}
+      )
+    end
+
     Enum.each extensions, fn extension ->
-      def type(unquote("." <> extension)), do: unquote(mimetype)
+      # Support getting a MIME type for extensions with and without a dot.
+      def mime_type(unquote(extension)) do
+        unquote({:ok, mimetype})
+      end
+      def mime_type(unquote("." <> extension)) do
+        unquote({:ok, mimetype})
+      end
+    end
+  end
+
+  @doc """
+  Thowing version of `mime_type/1`.
+  """
+  def mime_type!(extension) do
+    case mime_type(extension) do
+      {:ok, mime_type} -> mime_type
+      {:error, reason} -> raise ArgumentError, message: reason
+    end
+  end
+
+  @doc """
+  Get the MIME type for `extension`.
+
+  Extensions can contain periods or UPPERCASE characters. We take care of the
+  normalization. Returns {:ok, extension} on success, {:error, reason} on
+  failure.
+  """
+  def mime_type(extension) do
+    # Currently we are in the catch-all function. Since we want to support
+    # checking extensions that are uppercase, we have a check here to see if
+    # some characters of the extension are capitals. If they are, we can retry
+    # with a lower case, otherwise, we have not found a match and return an
+    # error.
+    unless String.downcase(extension) == extension do
+      extension
+      |> String.downcase
+      |> mime_type
+    else
+      {:error, "No extension matches"}
     end
   end
 end
--- test/mimex_test.exs
+++ test/mimex_test.exs
@@ -1,3 +1,20 @@
 defmodule MimexTest do
   use ExUnit.Case
+
+  test "find a few common file types" do
+    assert MIME.mime_type(".png") == {:ok, "image/png"}
+    assert MIME.mime_type(".pdf") == {:ok, "application/pdf"}
+    assert MIME.mime_type(".doc") == {:ok, "application/msword"}
+  end
+
+  test "handles duplicate extensions correctly" do
+    assert MIME.mime_type(".jpg")  == {:ok, "image/jpeg"}
+    assert MIME.mime_type(".jpeg") == {:ok, "image/jpeg"}
+    assert MIME.mime_type(".jpe")  == {:ok, "image/jpeg"}
+  end
+
+  test "handles UPPERCASE extensions correctly" do
+    assert MIME.mime_type(".JPEG") == {:ok, "image/jpeg"}
+    assert MIME.mime_type(".DOC")  == {:ok, "application/msword"}
+  end
 end
